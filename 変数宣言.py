b2b7873b0a56dbb16aa99cf1da4fe16e28779748
tabira2382 <tabira2382@gmail.com>
2020-10-09 00:29:01 +0900

    変数宣言

---
+++ 変数宣言.py
@@ -0,0 +1,15 @@
+num = 1
+name = 'Mike'
+is_ok = True
+
+num = name
+name = '1'
+new_num = int(name)
+
+print (num, type (num))
+print (name, type (name))
+print (is_ok, type (is_ok))
+
+print(num,type(num))
+
+print(new_num, type(num))
