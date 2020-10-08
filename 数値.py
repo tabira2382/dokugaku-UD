c042f263cbdc86bdea64eb0f483b23cacc5f5d4d
tabira2382 <tabira2382@gmail.com>
2020-10-09 00:52:46 +0900

    数値

---
+++ 数値.py
@@ -0,0 +1,25 @@
+print (2 + 2)
+print (5 - 2)
+print (5 * 6)
+print ((50 - 5) * 6)
+print (8 / 5)
+print (type (1))
+print (type (1.6))
+print (0.6)
+print (.6)
+print (17 / 3)
+print (17 // 3)
+print (17 % 3)
+print (5 * 5)
+print (5 ** 2)
+print (5 * 5 * 5 * 5 * 5)
+print (5 ** 5)
+
+x = 5
+print (5)
+y = 10
+print (x + y)
+
+pie = 3.14151515151515
+print (pie)
+print (round (pie, 2))
