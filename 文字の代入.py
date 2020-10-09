dda63f6b3aa930c22b244a21faeccff6c309e98c
tabira2382 <tabira2382@gmail.com>
2020-10-10 01:56:13 +0900

    文字を代入

---
+++ 文字の代入.py
@@ -0,0 +1,11 @@
+print ("a is {}".format ("a"))
+print ("a is {} {} {}".format (1, 2, 3, ))
+print ("a is {2} {1} {0}".format (1, 2, 3))
+print ("MY name is {0} {1}".format ("Jun", "Ogawa"))
+print ("My name is {name} {family}.Watashi ha {family} {name}".format (name="Jun", family="ogawa"))
+
+
+print(1)
+print('1')
+print(str(1))
+print(str(3.14))
