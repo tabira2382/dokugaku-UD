f8d215bf48c128c8cbb1f88416d467221e566535
tabira2382 <tabira2382@gmail.com>
2020-10-10 00:30:29 +0900

    文字列

---
+++ 文字列.py
@@ -0,0 +1,33 @@
+print ('hello')
+print ("hello")
+print ('I dont know')
+print ('I don\'t know')
+print ("say \"I don't know\"")
+
+print ('hello.\nHow are you?')
+print (r'C:\name\name')
+
+print ("""
+line1
+line2
+line3
+""")
+
+print ("############")
+print ("""\
+line1
+line2
+line3\
+""")
+print ("############")
+
+print ('Hi.' * 3 + 'Mike')
+
+print ('Py''thon')
+
+prefix = 'Py'
+print (prefix + 'thon')
+
+s = ('aaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
+     'bbbbbbbbbbbbbbbbbbbbbbbbbbbbb')
+print (s)
