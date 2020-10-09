609a3ba1f9cdc55fe7d681364e9e529ddf9c5c6e
tabira2382 <tabira2382@gmail.com>
2020-10-10 00:41:32 +0900

    文字列、インデックス、スライス、len

---
+++ 文字列とインデックス.py
@@ -0,0 +1,17 @@
+word = 'Python'
+print(word[0])
+print(word[1])
+print(word[-1])
+print(word[0:2])
+print(word[2:5])
+print(word[:2])
+print(word[2:])
+
+word = 'j' + word[1:]
+print(word)
+print(word)
+
+print(word[:])
+
+n = len(word)
+print(n)
